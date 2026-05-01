98ab1bfaf732e9def42b4c53381492a78b061525
Alwardas <aldspolytechnic@gmail.com>
2026-05-01 15:34:32 +0530

    chore: Clean up unwanted test scripts, logs, and IDE files

--- backend/read_output.py
+++
@@ -1,6 +0,0 @@
-with open('debug_subjects_output.txt', 'rb') as f:
-    content = f.read()
-    try:
-        print(content.decode('utf-16'))
-    except:
-        print(content.decode('utf-8'))
--- frontend/search_json.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('assets/data/json/cme.json', 'utf8');
-const data = JSON.parse(content);
-if (data.lesson_plans && data.lesson_plans['CM-303']) {
-    console.log("CM-303 lesson plan found in JSON");
-    console.log("Length:", data.lesson_plans['CM-303'].length);
-} else {
-    console.log("CM-303 NOT found in JSON lesson_plans");
-}
